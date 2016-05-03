e9bd3fa0229f3261603b82f749b90c16083c7be8
Colin Megill <colinmegill@gmail.com>
2016-05-02 21:17:15 -0400

    it talks!

---
+++ index.js
@@ -0,0 +1,24 @@
+var Botkit = require('botkit');
+var Witbot = require('witbot');
+
+var slackToken = process.env.SLACK_TOKEN;
+var witToken = process.env.WIT_TOKEN;
+
+var controller = Botkit.slackbot({
+  debug: false
+});
+
+controller.spawn({
+  token: slackToken
+}).startRTM(function (err, bot, payload) {
+  if (err) {
+    throw new Error('Error connecting to slack: ', err)
+  }
+  console.log('Connected to Slack');
+});
+
+var witbot = Witbot(witToken);
+
+controller.hears('.*', 'direct_message', function (bot, message) {
+  witbot.process(message.texts, message)
+})
